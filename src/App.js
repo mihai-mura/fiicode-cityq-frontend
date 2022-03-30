48394aeb79d8adb4226dfa4c07c00c499471ea3d
imEddie <kovereduard@gmail.com>
2022-03-30 20:33:05 +0300

    sidebar

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-	return <div className='App'>ciau mihai</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+
+
+function App() {
+	return <div>
+		<Navbar />
+	</div>;
+}
+
+export default App;
