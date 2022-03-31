09f0ac1ce43dbe4957f3c4f456b0305c5ece72ee
imEddie <kovereduard@gmail.com>
2022-03-31 14:12:32 +0300

    created routes and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Navbar from "./components/Navbar";
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Explore from "./pages/Explore";
+import Sidebar from './components/Sidebar';
 
 function App() {
-	return <div>
-		<Navbar />
-	</div>;
+	return <>
+		<Router>
+			<Sidebar>
+				<h2>mihai te iubesc</h2>
+				<Routes>
+					<Route path="/" element={<Explore />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+			</Sidebar>
+		</Router>
+	</>;
 }
 
 export default App;
