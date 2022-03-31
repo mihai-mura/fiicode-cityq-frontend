629ce8feea6fb0ac69759bbb16519ad26f4614dc
mihai-mura <mihai.mura2@gmail.com>
2022-03-31 17:10:32 +0300

    main page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Dashboard from "./pages/Dashboard";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
-import Explore from "./pages/Explore";
-import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Explore from './pages/Explore';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
-	return <>
-		<Router>
-			<Sidebar>
+	return (
+		<div className='App'>
+			<Router>
+				<Sidebar />
 				<h2>mihai te iubesc</h2>
 				<Routes>
-					<Route path="/" element={<Explore />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-			</Sidebar>
-		</Router>
-	</>;
+					<Route path='/' element={<Explore />} />
+					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/users' element={<Users />} />
+					<Route path='/settings' element={<Settings />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
