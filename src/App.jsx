566875ddc31a99a608827ba8436a98f2ac347466
mihai-mura <mihai.mura2@gmail.com>
2022-03-31 20:02:03 +0300

    created auth modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import Users from './pages/Users';
 import Settings from './pages/Settings';
 import Explore from './pages/Explore';
 import Sidebar from './components/Sidebar/Sidebar';
+import Authentification from './components/Authentification/Authentification';
 
 function App() {
 	return (
 		<div className='App'>
 			<Router>
+				{/*//! plug redux */}
+				<Authentification open={null} close={null} type={0} /> {/*1 register | 0 login */}
 				<Sidebar />
-				<h2>mihai te iubesc</h2>
-				<Routes>
-					<Route path='/' element={<Explore />} />
-					<Route path='/dashboard' element={<Dashboard />} />
-					<Route path='/users' element={<Users />} />
-					<Route path='/settings' element={<Settings />} />
-				</Routes>
+				<div className='main'>
+					<h2>mihai te iubesc</h2>
+					<Routes>
+						<Route path='/' element={<Explore />} />
+						<Route path='/dashboard' element={<Dashboard />} />
+						<Route path='/users' element={<Users />} />
+						<Route path='/settings' element={<Settings />} />
+					</Routes>
+				</div>
 			</Router>
 		</div>
 	);
