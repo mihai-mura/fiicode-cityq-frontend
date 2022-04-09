8909ccc7d6c503bfa3b84a9eb58b632aea44e96e
mihai-mura <mihai.mura2@gmail.com>
2022-04-09 14:43:27 +0300

    fixed profile image not  loading url

--- src/components/UrlFromNodeImg.jsx
+++ src/components/UrlFromNodeImg.jsx
@@ -5,9 +5,14 @@ const UrlFromNodeImg = (props) => {
 	const [url, setUrl] = useState('');
 	useEffect(() => {
 		(async () => {
-			const res = await fetch(props.imageurl);
-			const res2 = await res.text();
-			setUrl(res2);
+			while (true) {
+				const res = await fetch(props.imageurl);
+				if (res.status === 200) {
+					const res2 = await res.text();
+					setUrl(res2);
+					break;
+				}
+			}
 		})();
 	}, [props.imageurl]);
 	// eslint-disable-next-line jsx-a11y/alt-text
