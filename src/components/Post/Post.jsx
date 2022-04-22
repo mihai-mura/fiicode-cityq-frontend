c6182b6af1ec4a418b25e9ff0f1dae17499d4caf
mihai-mura <mihai.mura2@gmail.com>
2022-04-22 10:24:20 +0300

    added post carousel

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,5 +1,7 @@
 import './Post.scss';
 import { IconArrowBigDownLine, IconArrowBigUpLine } from '@tabler/icons';
+import 'swiper/scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Post = (props) => {
 	//! carousel
@@ -10,8 +12,14 @@ const Post = (props) => {
 					<div className='post-user'>{props.user}</div>
 					<div className='post-city'>{props.city}</div>
 				</div>
-				<div className='postcarousel-container'>
-					<img src={props.fileUrls[0]} alt='post' />
+				<div className='post-carousel-container'>
+					<Swiper autoHeight slidesPerView={1}>
+						{props.fileUrls.map((file, index) => (
+							<SwiperSlide className='carousel-slide' key={index}>
+								{file.includes('.mp4?') ? <video controls src={file} /> : <img src={file} alt='post' />}
+							</SwiperSlide>
+						))}
+					</Swiper>
 				</div>
 				<div className='post-title'>{props.title}</div>
 				<div className='post-description'>{props.description}</div>
